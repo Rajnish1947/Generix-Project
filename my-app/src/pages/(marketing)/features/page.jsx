ca225331a7a3d1142a75eefbae22ab938b0e48f8
Rajnish1947 <kumarrajnish28443@gmail.com>
2026-02-16 18:20:44 +0530

    upate

--- my-app/src/pages/(marketing)/features/page.jsx
+++
@@ -1,5 +0,0 @@
-export default function FeaturesPage() {
-  return (
-    <h1 className="text-3xl font-bold">Features</h1>
-  );
-}
--- my-app/src/pages/(marketing)/pricing/page.jsx
+++
@@ -1,5 +0,0 @@
-export default function PricingPage() {
-  return (
-    <h1 className="text-3xl font-bold">Pricing</h1>
-  );
-}
