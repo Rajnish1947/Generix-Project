6320967379030d56a31272d46424465028d8e98c
Rajnish1947 <kumarrajnish28443@gmail.com>
2026-03-02 11:18:28 +0530

    Rajnish Kumar

--- my-apps/src/components/Mainsection/Hero.jsx
+++ my-apps/src/components/Mainsection/Hero.jsx
@@ -1,56 +1,5 @@
 
 
-// "use client";
-// import React from "react";
-// import { useTheme } from "@/Context/ThemeContext/page";
-// const Hero = () => {
-//   const { dark, setDark } = useTheme();
-//   return (
-//     <section
-//       className="relative bg-white min-h-[65vh] lg:min-h-screen bg-cover bg-center"
-//       style={{
-//         backgroundImage: dark
-//           ? "url('https://www.mintlify.com/startups/hero-dark.svg')"
-//           : "url('https://www.mintlify.com/startups/hero-light.svg')",
-//       }}
-//     >
-//       {/* Overlay */}
-//       <div
-//         className={`absolute inset-0 bg-gradient-to-t ${dark ? "from-black/95 via-gray-900/10 to-transparent" : "from-white/90 via-white/10 to-transparent"}`}
-//       ></div>
-
-//       {/* Content */}
-//       <div
-//         className="relative z-10 max-w-[69rem] mx-auto  flex flex-col px-4 sm:px-6 lg:px-10
-// pt-38 sm:pt-29 md:pt-36 
-// pb-16 md:pb-28 lg:flex-row items-start lg:items-center gap-12"
-//       >
-//         <div className="lg:w-1/2  flex flex-col items-center text-center md:items-start md:text-left  ">
-//           <h1 className="text-3xl max-w-[50rem] text-text-main hero-title leading-[110%] text-balance text-center sm:text-left sm:text-3xl md:text-4xl lg:text-5xl font-bold  text-white">
-//             Built for startups <br /> and scaleups
-//           </h1>
-
-//           <p className="mt-6 max-w-md md:max-w-xl text-stone-50 text-sm md:text-[16px]">
-//           Ambitious startups build world-class experiences with Mintlify, scaling from pre-seed to billion dollar valuations.
-//           </p>
-
-//           <div className="mt-8 flex flex-col sm:flex-row gap-4">
-//             <button className="bg-white text-gray-900 px-30 py-1 md:px-7 md:py-2 rounded-3xl font-medium hover:bg-gray-100">
-//               Get Started
-//             </button>
-
-//             <button className="text-white flex items-center gap-2 font-medium">
-//               Apply for Startup Program <span className="text-xl">&gt;</span>
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Hero;
-
 "use client";
 import React from "react";
 import { useTheme } from "@/Context/ThemeContext/page";
@@ -79,7 +28,7 @@ const Hero = () => {
       {/* Content */}
       <div
         className="
-        relative z-50 
+        relative z-40 
         max-w-[69rem] mx-auto
         flex flex-col
         px-4 sm:px-6 lg:px-10
--- my-apps/src/layouts/Navbar.jsx
+++ my-apps/src/layouts/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { Equal, X } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { useTheme } from "@/Context/ThemeContext/page";
 
 export default function Navbar({ blogpage }) {
@@ -206,7 +206,7 @@ export default function Navbar({ blogpage }) {
                      : "text-primary "
              }`}
           >
-            {open ? <X size={28} /> : <Equal size={28} />}
+            {open ? <X size={28} /> : <Menu size={28} />}
           </button>
         </div>
       </nav>
